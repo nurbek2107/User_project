d1c018336f3572a9d0a49b076670efd03ba707aa
nurbek2107 <abduaxatov002@gmail.com>
2024-07-24 10:38:15 +0500

    main
    Signed-off-by: nurbek2107 <abduaxatov002@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
-/** @type {import('tailwindcss').Config} */
+const withMT = require("@material-tailwind/react/utils/withMT");
+
 module.exports = {
+  daisyui: {
+    themes: ["light"],
+  },
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "my-bg-gif": "url(../../public/assets/gif/bg-gif.gif)",
+        "card-gif": "url(../../public/assets/gif/card-gif.gif)",
+        "login-circle": "url(../../public/assets/bg-circle-login.jpg)",
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+    // Add any additional plugins here if needed
+  ],
 };
