be7b5a1cacea1752dfbada944a3d77123aa8ec92
Nurbek <161306161+nurbek2107@users.noreply.github.com>
2024-08-09 21:01:03 +0500

    main

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['example.com'], // Replace with your domain
+    },
+  };
+  
+  module.exports = nextConfig;
+  
