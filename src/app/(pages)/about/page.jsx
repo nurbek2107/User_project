00fb5e89e1fe07ceea961228fa2dcfe6c60cc4a7
Nasrddinovvv1508 <najmiddinnasriddinov5@gmail.com>
2024-08-09 12:45:37 +0500

    our goals

--- src/app/(pages)/about/page.jsx
+++ src/app/(pages)/about/page.jsx
@@ -19,7 +19,7 @@ function About() {
         </div>
       </section>
 
-      <section className='main-container' style={{ marginBottom: `80px` }}>
+      <section className='main-container' style={{ marginBottom: `200px` }}>
         <div className='mb-[40px]'>
           <h1 className='text-4xl font-semibold'>Achievements</h1>
           <p className='text-sm text-[#59595a]'>
@@ -29,7 +29,7 @@ function About() {
 
         <div className='grid grid-cols-2 gap-4'>
           <div className='bg-white p-[25px] rounded-md'>
-            <img className='w-[43px]' src="../assets/krono.svg" alt="krono" />
+            <img className='w-[43px] select-none' src="../assets/krono.svg" alt="krono" />
             <h4 className='text-[20px] font-medium mt-[22px] mb-[8px]'>Trusted by Thousands</h4>
             <p className='text-[#59595a] text-[16px]'>
               We have successfully served thousands of students, helping them unlock their potential and achieve their career goals.
@@ -37,7 +37,7 @@ function About() {
           </div>
 
           <div className='bg-white p-[25px] rounded-md'>
-            <img className='w-[43px]' src="../assets/krono.svg" alt="krono" />
+            <img className='w-[43px] select-none' src="../assets/krono.svg" alt="krono" />
             <h4 className='text-[20px] font-medium mt-[22px] mb-[8px]'>Trusted by Thousands</h4>
             <p className='text-[#59595a] text-[16px]'>
               We have successfully served thousands of students, helping them unlock their potential and achieve their career goals.
@@ -45,7 +45,7 @@ function About() {
           </div>
 
           <div className='bg-white p-[25px] rounded-md'>
-            <img className='w-[43px]' src="../assets/krono.svg" alt="krono" />
+            <img className='w-[43px] select-none' src="../assets/krono.svg" alt="krono" />
             <h4 className='text-[20px] font-medium mt-[22px] mb-[8px]'>Trusted by Thousands</h4>
             <p className='text-[#59595a] text-[16px]'>
               We have successfully served thousands of students, helping them unlock their potential and achieve their career goals.
@@ -53,10 +53,54 @@ function About() {
           </div>
 
           <div className='bg-white p-[25px] rounded-md'>
-            <img className='w-[43px]' src="../assets/krono.svg" alt="krono" />
+            <img className='w-[43px] select-none' src="../assets/krono.svg" alt="krono" />
             <h4 className='text-[20px] font-medium mt-[22px] mb-[8px]'>Trusted by Thousands</h4>
             <p className='text-[#59595a] text-[16px]'>
               We have successfully served thousands of students, helping them unlock their potential and achieve their career goals.
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <section className='main-container' style={{ marginBottom: `80px` }}>
+        <div className='mb-[40px]'>
+          <h1 className='text-4xl font-semibold mb-[10px]'>Our Goals</h1>
+          <p className='text-sm text-[#59595a]'>
+            At SkillBridge, our goal is to empower individuals from all backgrounds to thrive in the world of design and development. We believe that education should be accessible and transformative, enabling learners to pursue their passions and make a meaningful impact.
+            Through our carefully crafted courses, we aim to
+          </p>
+        </div>
+
+        <div className='grid grid-cols-2 gap-4'>
+          <div className='bg-white p-[25px] rounded-md'>
+            <img className='w-[43px] select-none' src="../assets/icon-container.png" alt="icon" />
+            <h4 className='text-[20px] font-medium mt-[22px] mb-[8px]'>Provide Practical Skills</h4>
+            <p className='text-[#59595a] text-[16px]'>
+              We focus on delivering practical skills that are relevant to the current industry demands. Our courses are designed to equip learners with the knowledge and tools needed to excel in their chosen field.
+            </p>
+          </div>
+
+          <div className='bg-white p-[25px] rounded-md'>
+            <img className='w-[43px] select-none' src="../assets/icon-container-1.png" alt="icon" />
+            <h4 className='text-[20px] font-medium mt-[22px] mb-[8px]'>Foster Creative Problem-Solving</h4>
+            <p className='text-[#59595a] text-[16px]'>
+              We encourage creative thinking and problem-solving abilities, allowing our students to tackle real-world challenges with confidence and innovation.
+            </p>
+          </div>
+
+          <div className='bg-white p-[25px] rounded-md'>
+            <img className='w-[43px] select-none' src="../assets/icon-container-2.png" alt="icon" />
+            <h4 className='text-[20px] font-medium mt-[22px] mb-[8px]'>Promote Collaboration and Community</h4>
+            <p className='text-[#59595a] text-[16px]'>
+              We believe in the power of collaboration and peer learning. Our platform fosters a supportive and inclusive community where learners can connect, share insights, and grow together.
+            </p>
+          </div>
+
+          <div className='bg-white p-[25px] rounded-md'>
+            <img className='w-[43px] select-none' src="../assets/icon-container-3.png" alt="icon" />
+            <h4 className='text-[20px] font-medium mt-[22px] mb-[8px]'>Stay Ahead of the Curve</h4>
+            <p className='text-[#59595a] text-[16px]'>
+              The digital landscape is constantly evolving, and we strive to stay at the forefront of industry trends. We regularly update our course content to ensure our students receive the latest knowledge and skills.
             </p>
           </div>
         </div>
