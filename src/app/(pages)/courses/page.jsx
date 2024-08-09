7aa74c3a0024b3fbf3f2fb1adbe7509cbd869982
Nurbek <161306161+nurbek2107@users.noreply.github.com>
2024-08-09 12:28:09 +0500

    version .8

--- src/app/(pages)/courses/page.jsx
+++ src/app/(pages)/courses/page.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function Courses() {
   return (
-    <div>Courses</div>
+    <div className='main-container p-4'>
+      <h1 className='text-3xl font-bold'>Kurslar</h1>
+      <div>
+        
+      </div>
+    </div>
   )
 }
 
