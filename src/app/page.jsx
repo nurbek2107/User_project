796feedc62ba47cc30ce6bf3f43ad4886becbe17
Nurbek <161306161+nurbek2107@users.noreply.github.com>
2024-08-12 16:45:19 +0500

    version

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import HomeCards from "@/components/HomeCards";
+function App() {
+  return (
+    <>
+      <div className='main-container pt-[70px]'>
+          <HomeCards />
+      </div>
+    </>
+  )
+}
+
+export default App
