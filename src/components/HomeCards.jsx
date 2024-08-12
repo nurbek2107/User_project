0a5aaaec8b821b78d33fa68461c97caeca2def11
Nurbek <161306161+nurbek2107@users.noreply.github.com>
2024-08-12 20:23:52 +0500

    version 5

--- src/components/HomeCards.jsx
+++ src/components/HomeCards.jsx
@@ -1,20 +1,11 @@
 import React from 'react';
-import "./HomeCards.css"
+import "./components.css"
 function HomeCards() {
   return (
     <>
-      <div className="main-container shadow-md rounded-xl h-[500px]">
-      <main className="flex items-end  text-[35px] font-bold  ">
-      <p>Yangi imkoniyatlarni oching va</p>
-      <section className="animation ">
-      <div className="first"><div>Backend</div></div>
-      <div className="second"><div>Frontend</div></div>
-      <div className="third"><div>Flatter</div></div>
-      </section>
-  
-      </main>
-      <p className='text-[35px] font-bold'> dasturlashni o‘rganing</p>
-      </div>
+    <div class="animated-text">
+    Yangi imkoniyatlarni oching va <span></span> <br /> dasturlashni o‘rganing
+    </div>
     </>
   )
 }
